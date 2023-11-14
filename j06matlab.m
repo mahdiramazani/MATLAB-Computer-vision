9f127e8dafe9c02d26c78e4628a72beccc536780
mahdiramazani <mahdiramazani1281@gmail.com>
2023-11-14 14:22:07 +0330

    a

--- j06matlab.m
+++ j06matlab.m
@@ -1,2 +1,4 @@
 pic=imread("E://pic/women-color.jpg");
-mask=fspecial('disk',2);
+mask=fspecial('average',[2 5]);
+subplot(2,2,1),imshow(pic),title("original")
+subplot(2,2,2),imshow(imfilter(pic,mask)),title("[2 5]")
