abde7e67109cdcaf5a35e3bf084f352bca545ca8
mahdiramazani <mahdiramazani1281@gmail.com>
2023-11-14 15:25:52 +0330

    dd

--- j06matlab.m
+++ j06matlab.m
@@ -1,4 +1,4 @@
-pic=imread("E://pic/noice_women.jpg");
+pic=imread("E://pic/lake.jpeg");
 mask=fspecial('average',[2 5]);
 mask_2=fspecial('average',[3 7]);
 mask_3=fspecial('average',[4 8]);
@@ -18,16 +18,16 @@ subplot(1,3,2),imshow(imfilter(pic,mask_gaussian));title("gaussian")
 subplot(1,3,3),imshow(imfilter(pic,mask_disk));title("disk")
 %================== 9999999999999999999999999999999999999
 figure;
-
-subplot(2,2,1),imshow(imfilter(pic,mask,3)),title("x");
-subplot(2,2,2),imshow(imfilter(pic,mask,"symmetric")),title("symmetric");
-subplot(2,2,3),imshow(imfilter(pic,mask,"replicate")),title("replicate");
-subplot(2,2,4),imshow(imfilter(pic,mask_2,"circular")),title("circular");
+pic_3=rgb2gray(pic);
+subplot(2,2,1),imshow(imfilter(pic_3,mask,3)),title("x");
+subplot(2,2,2),imshow(imfilter(pic_3,mask,"symmetric")),title("symmetric");
+subplot(2,2,3),imshow(imfilter(pic_3,mask,"replicate")),title("replicate");
+subplot(2,2,4),imshow(imfilter(pic_3,mask_2,"circular")),title("circular");
 
 %================== 10000000000000000000000000000000
 
 figure;
-pic_2=imread("E://pic/Afl808ace Bird lovers kiss Animal Photography Picture-96587254,2017,Yunnan Dali,Animal Photography Black And White,Animal photography beautiful birds_bird Photography beautiful cute.jpg");
+pic_2=imread("baby.jpg");
 mask_sh=fspecial('laplacian',0.5);
 mask_log=fspecial('log',5,1);
 mask_un=fspecial('unsharp');
