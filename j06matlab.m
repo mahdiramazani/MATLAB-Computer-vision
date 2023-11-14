310b493d90a6e2ce2166d1ef9aa8f78e4612f174
mahdiramazani <mahdiramazani1281@gmail.com>
2023-11-14 15:05:48 +0330

    dd

--- j06matlab.m
+++ j06matlab.m
@@ -16,13 +16,25 @@ mask_disk=fspecial('disk');
 subplot(1,3,1),imshow(imfilter(pic,mask_average));title("average")
 subplot(1,3,2),imshow(imfilter(pic,mask_gaussian));title("gaussian")
 subplot(1,3,3),imshow(imfilter(pic,mask_disk));title("disk")
-
+%================== 9999999999999999999999999999999999999
 figure;
 
 subplot(2,2,1),imshow(imfilter(pic,mask,3)),title("x");
 subplot(2,2,2),imshow(imfilter(pic,mask,"symmetric")),title("symmetric");
 subplot(2,2,3),imshow(imfilter(pic,mask,"replicate")),title("replicate");
 subplot(2,2,4),imshow(imfilter(pic,mask_2,"circular")),title("circular");
+
+%================== 10000000000000000000000000000000
+
+figure;
+pic_2=imread("E://pic/Afl808ace Bird lovers kiss Animal Photography Picture-96587254,2017,Yunnan Dali,Animal Photography Black And White,Animal photography beautiful birds_bird Photography beautiful cute.jpg");
+mask_sh=fspecial('laplacian',0.5);
+mask_log=fspecial('log',5,1);
+mask_un=fspecial('unsharp');
+imshow(pic_2),title("original");figure
+subplot(1,3,1),imshow(imfilter(pic_2,mask_sh)),title("laplacian")
+subplot(1,3,2),imshow(imfilter(pic_2,mask_log)),title("log")
+subplot(1,3,3),imshow(imfilter(pic_2,mask_un)),title("unsharp")
 
 
 
